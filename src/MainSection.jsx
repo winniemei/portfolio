df668b54bc112510907ddd53f00e4a2b221445e1
winniemei <wmei91@gmail.com>
2023-10-16 23:46:52 -0400

    added bio

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -2,6 +2,23 @@ export default function MainSection() {
 	return (
 		<div id="main-section">
 			<h1>Welcome to Winnie's World</h1>
+			<div id="bio">
+				<p>
+					📈💸💻 I am a software engineer with a unique blend of
+					expertise, drawing inspiration from my background at the
+					intersection of Wall Street and Silicon Valley. I started
+					off my career in investment banking and I am now at a
+					fintech unicorn that is building the operating system for
+					the financial world. I explore my love of numbers and logic
+					through different careers in finance, technology, and data.
+				</p>
+				<p>
+					👩🏻‍💻🦋 I am passionate about harnessing the power of
+					technology to solve complex problems and drive innovation.
+					Let's collaborate to build elegant software and transform
+					the world.
+				</p>
+			</div>
 			<div></div>
 			{/* <iframe
 				src="https://studio-drink.onrender.com/"
