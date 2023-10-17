bdcf12250ef27f090813b665cf892234cd79034e
winniemei <wmei91@gmail.com>
2023-10-16 23:39:37 -0400

    add to main section

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -2,9 +2,7 @@ export default function MainSection() {
 	return (
 		<div id="main-section">
 			<h1>Welcome to Winnie's World</h1>
-			<div>
-				<h1>Enter</h1>
-			</div>
+			<div></div>
 			{/* <iframe
 				src="https://studio-drink.onrender.com/"
 				width="auto"
